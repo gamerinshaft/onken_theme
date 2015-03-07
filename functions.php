787b42f7e01a3b601b8ead67ee71fa182e9c92b3
j.taneichi <j.tane3ma@gmail.com>
2015-03-07 20:21:06 +0900

    認証の実装

--- functions.php
+++ functions.php
@@ -1,1 +1,24 @@
 <?php add_theme_support('post-thumbnails'); ?>
+<?php
+function login_logo() {
+echo '<style type="text/css">
+#login h1 a {
+background: url('.get_bloginfo('template_directory').'/img/login.png) no-repeat;
+background-size: contain;
+width: 150px;
+height: 150px;
+border: double 4px #555;
+border-radius: 50%;
+}
+#login h1:before {
+  display: block;
+  content: "音楽研究会ログインページ";
+  margin-bottom: 16px;
+  -webkit-font-smoothing: antialiased;
+  font-weight: normal;
+  color: #555;
+}
+</style>';
+}
+add_action('login_head', 'login_logo');
+?>
