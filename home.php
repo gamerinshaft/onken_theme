8b3e39c1613ebef0ae87d567a4f6a922cab2ce27
j.taneichi <j.tane3ma@gmail.com>
2015-03-31 04:14:15 +0900

    update

--- home.php
+++ home.php
@@ -3,6 +3,11 @@
 Template Name: home
 */
 ?>
+<?php
+  $image_id = get_post_thumbnail_id();
+  $image_url = wp_get_attachment_image_src($image_id, true);
+  $image_url[0] = empty($image_url[0])?get_bloginfo('stylesheet_directory') . '/img/programming_back.jpg':$image_url[0];
+?>
 <!DOCTYPE html>
 <html lang="ja">
   <head>
@@ -30,13 +35,62 @@ Template Name: home
         <img src="<?php bloginfo('stylesheet_directory'); ?>/img/top-header.png">
       </div>
     </div>
-    <?php
-      $image_id = get_post_thumbnail_id();
-      $image_url = wp_get_attachment_image_src($image_id, true);
-      $image_url[0] = empty($image_url[0])?get_bloginfo('stylesheet_directory') . '/img/programming_back.jpg':$image_url[0];
-    ?>
-    <?php the_post() ?>
-    <!-- <?php the_title(); ?> -->
-    <?php the_content(); ?>
+    <div class="main">
+      <div class="items">
+        <div class="row">
+          <div class="col-xs-3">
+            <div class="item">
+              <i class="fa fa-question-circle"></i>
+              <p>アバウト</p>
+            </div>
+          </div>
+          <div class="col-xs-3">
+            <div class="item">
+              <i class="fa fa-bell-o"></i>
+              <p>新入生へ</p>
+            </div>
+          </div>
+          <div class="col-xs-3">
+            <div class="item">
+              <i class="fa fa-desktop"></i>
+              <p>実装した機能</p>
+            </div>
+          </div>
+          <div class="col-xs-3">
+            <div class="item">
+              <i class="fa fa-send"></i>
+              <p>コンタクト</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer class="info">
+      <div class="container">
+        <div class="row">
+          <div class="left-art col-sm-5 col-xs-6">
+            <div class="table">
+              <div class="table-cell">
+                <div class="info-icon">
+                  <i class="fa fa-info"></i>
+                </div>
+                <p class="info-title">
+                  新着情報
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="right-art col-sm-7 col-xs-6">
+            <div class="table">
+              <div class="table-cell">
+                <?php the_post() ?>
+                <!-- <?php the_title(); ?> -->
+                <?php the_content(); ?>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
   </body>
 </html>
